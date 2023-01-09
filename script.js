90754c8a89d9b414004544cc68fbc2fc606ebe6a
gabriel-victor9333 <gabrielvictor933@gmail.com>
2023-01-09 09:48:13 -0300

    aplicando correções da avaliação de código

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function selecionar(id) {
-    let ter = {}
+    let ter = {};
 
 
-    if (id[1] == 'f') {
-        ter = document.querySelector("#pratos .selecionado")
+    if (id[1] === 'f') {
+        ter = document.querySelector("#pratos .selecionado");
 
-    } else if (id[1] == 'b') {
-        ter = document.querySelector("#bebidas .selecionado")
+    } else if (id[1] === 'b') {
+        ter = document.querySelector("#bebidas .selecionado");
     } else {
-        ter = document.querySelector("#sobremesas .selecionado")
+        ter = document.querySelector("#sobremesas .selecionado");
     }
-
-    //let ter = document.querySelector(".selecionado");
 
     if (ter !== null) {
         ter.classList.remove("selecionado");
@@ -21,79 +19,75 @@ function selecionar(id) {
     ter.classList.add("selecionado");
 
 
+    let quantSelecionados = 3;
+    if (document.getElementsByClassName("selecionado").length === quantSelecionados) {
 
-    if (document.getElementsByClassName("selecionado").length == 3) {
-
-        habilitar_fechar_pedido();
+        habilitarFecharPedido();
     }
 
 }
 
 
-function habilitar_fechar_pedido() {
-    let fechar = document.querySelector("#fechar");
+function habilitarFecharPedido() {
+    const fechar = document.querySelector("#fechar");
     fechar.disabled = false;
-    fechar.classList.add("finalizar")
-    fechar.innerHTML = "Fechar pedido"
+    fechar.classList.add("finalizar");
+    fechar.innerHTML = "Fechar pedido";
 }
 
-function fechar_pedido() {
-
+function fecharPedido() {
     //função que obtem preços e tamanha dos pedidos
 
-    //let ped = document.querySelectorAll('.selecionado');
-    let preco = pegarPreco();
-    let tam = pegarTamanho();
-    let link = gerar_link(preco, tam);
-    abrir_pagina(link)
+    const preco = pegarPreco();
+    const tam = pegarTamanho();
+    const link = gerarLink(preco, tam);
+    abrirPagina(link);
 
 }
 
 function cancelar() {
-    let conf = document.querySelector(".confirmar");
+    const conf = document.querySelector(".confirmar");
     conf.style.display = "none";
 }
 
-function gerar_link(preco, tam) {
+function gerarLink(preco, tam) {
     let texto = "Olá, gostaria de fazer o pedido:";
-    let prato = "- Prato: Frango Yin Yang" + tam[0];
-    let bebida = "- Bebida: Coquinha Gelada" + tam[1];
-    let sobre = "- Sobremesa: Pudim" + tam[2];
-    let total = parseFloat(preco[0]) + parseFloat(preco[1]) + parseFloat(preco[2]);
-    let nome = prompt("Qual o seu nome?");
-    let endereco = prompt("Qual o seu endereço?");
+    const prato = "- Prato: Frango Yin Yang" + tam[0];
+    const bebida = "- Bebida: Coquinha Gelada" + tam[1];
+    const sobre = "- Sobremesa: Pudim" + tam[2];
+    const total = parseFloat(preco[0]) + parseFloat(preco[1]) + parseFloat(preco[2]);
+    const nome = prompt("Qual o seu nome?");
+    const endereco = prompt("Qual o seu endereço?");
 
 
     texto = `${encodeURIComponent(texto)}%0A${encodeURIComponent(prato)}%0A${encodeURIComponent(bebida)}%0A${encodeURIComponent(sobre)}%0A%0A${encodeURIComponent("TOTAL: R$" + total)}%0A%0A${encodeURIComponent("nome:" + nome)}%0A${encodeURIComponent("Endereço:" + endereco)}`;
 
-    let link = `https://wa.me/55015981353028?text=${texto}`;
+    return `https://wa.me/55015981353028?text=${texto}`;
 
-    return link;
 }
 
 //funcao ativado quando clicamos em fechar o pedido
 function confirmar() {
 
-    let preco = pegarPreco();
+    const preco = pegarPreco();
     let total = 0;
     for (let i = 0; i < 3; i++) {
-        total = total + parseFloat(preco[i])
+        total = total + parseFloat(preco[i]);
     }
 
 
-    let conf = document.querySelector(".confirmar");
-    let test = conf.querySelectorAll('#precos');
-    test[0].innerHTML = `R\$ ${preco[0]
-        }`
+    const conf = document.querySelector(".confirmar");
+    const test = conf.querySelectorAll('#precos');
+    test[0].innerHTML = `R\$ ${preco[0]}`;
     test[1].innerHTML = "R$ " + preco[1];
     test[2].innerHTML = "R$ " + preco[2];
-    test[3].innerHTML = `R\$ ${total}`
+    test[3].innerHTML = `R\$ ${total}`;
     conf.style.display = "flex";
 
 }
 
 function pegarPreco() {
-    let ped = document.querySelectorAll('.selecionado');
+    const ped = document.querySelectorAll('.selecionado');
     let preco = [];
     for (let i = 0; i < 3; i++) {
         preco[i] = ped[i].getElementsByTagName('span').namedItem('preco').innerHTML;
@@ -103,7 +97,7 @@ function pegarPreco() {
 }
 
 function pegarTamanho() {
-    let ped = document.querySelectorAll('.selecionado');
+    const ped = document.querySelectorAll('.selecionado');
 
     let tam = [];
     for (let i = 0; i < 3; i++) {
@@ -114,7 +108,7 @@ function pegarTamanho() {
 }
 
 
-function abrir_pagina(link) {
+function abrirPagina(link) {
 
     var win = window.open(link, '_blank');
     win.focus();
